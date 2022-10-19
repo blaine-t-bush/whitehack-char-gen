cc1709fb90f0e4763a00a99009d530bef4f62d11
blaine-t-bush <blaine-t-bush@users.noreply.github.com>
2022-10-19 14:11:58 +0000

    deploy: 8ad8982861a6eb98080358d8a6d038d828e2edb8

--- src/lib/data/specieses.ts
+++
@@ -1,7 +0,0 @@
-const specieses = [
-  { name: "Human", languages: ["Low Imperial"], isDefault: true },
-  { name: "Dwarf", languages: ["Low Imperial", "Khadish"], isDefault: false },
-  { name: "Elf", languages: ["Low Imperial", "High Imperial"], isDefault: false },
-];
-
-export { specieses }
--- src/lib/interfaces/armor.ts
+++
@@ -1,8 +0,0 @@
-interface Armor {
-  name: string;
-  armorClass: number;
-  cost: number;
-  allowedClasses: string[];
-}
-
-export type { Armor }
